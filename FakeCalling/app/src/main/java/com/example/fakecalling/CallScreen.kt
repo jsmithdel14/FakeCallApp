94fdd9ff136426c137c864867826c9d5950bc506
Josh Smith <jsmithdel14@gmail.com>
2022-10-25 14:19:48 -0600

    Intial Commit(Nothing Changed)

---
+++ FakeCalling/app/src/main/java/com/example/fakecalling/CallScreen.kt
@@ -0,0 +1,11 @@
+package com.example.fakecalling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CallScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call_screen)
+    }
+}
